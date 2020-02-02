42d68fb4149304b291653e6bd0f768a07fef3c13
Nate <lao.nathan@yahoo.com>
2020-02-02 14:48:39 -0500

    tag testing

--- buildscript.sh
+++ buildscript.sh
@@ -3,5 +3,8 @@
 echo $WORKSPACE
 
 touch output.txt
-echo "The current directory=$WORKSPACE" > output.txt
+echo "The current directory=$WORKSPACE" >> output.txt
+echo "This should be output of tag potato" >> output.txt
+
+exit 0
 
