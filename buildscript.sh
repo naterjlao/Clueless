ef49aabeaa8ddea0819a0370368eda63608e1f0c
Nate <lao.nathan@yahoo.com>
2020-02-02 14:49:43 -0500

    different tag

--- buildscript.sh
+++ buildscript.sh
@@ -4,7 +4,7 @@ echo $WORKSPACE
 
 touch output.txt
 echo "The current directory=$WORKSPACE" >> output.txt
-echo "This should be output of tag potato" >> output.txt
+echo "This should be output of tag fries" >> output.txt
 
 exit 0
 
