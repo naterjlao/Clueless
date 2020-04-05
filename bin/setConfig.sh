ce92b0a038339892701e414abb4ad330f00311f9
Nate Lao <lao.nathan@yahoo.com>
2020-04-05 15:10:24 -0400

    create configuration command line utilities, unfinished

---
+++ bin/setConfig.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+################################################################################
+# File: setConfig.sh
+# Language: Bash
+# Author: Nate Lao (nlao1@jh.edu)
+# Date Created: 4/5/2020
+# Description:
+#          Generates the appropriate configuration for the FrontEnd, BackEnd and
+#          ServerSide based on the central configuration in
+#          /opt/clueless/etc/clueless.config
+################################################################################
+
+# FrontEnd Configuration
+
+
+# ServerSide Configuration
+
+
+# BackEnd Configuration
